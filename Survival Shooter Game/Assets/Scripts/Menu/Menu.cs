c87e2c8ae3d9ff06a321e48758af15edd97e49dd
jenktam <jennifer.k.tam@gmail.com>
2017-06-16 17:55:49 -0700

    feat(Menu): Menu page updated with Play Game + Options Buttons

---
+++ Survival Shooter Game/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour 
+{
+    public Canvas MenuCanvas;
+    public Canvas OptionsCanvas;
+
+    void Awake()
+    {
+        OptionsCanvas.enabled = false;
+
+    }
+
+    public void OptionsOn()
+    {
+        OptionsCanvas.enabled = true;
+        MenuCanvas.enabled = false;
+    }
+
+    public void ReturnOn()
+    {
+        OptionsCanvas.enabled = false;
+        MenuCanvas.enabled = true;
+    }
+
+    public void LoadOn()
+    {
+        Application.LoadLevel(1);
+    }
+}
